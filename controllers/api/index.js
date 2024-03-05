45f9831bab35a2541949b4ef0e90647df5696206
Chris Stevenson <chrislstevenson@gmail.com>
2024-03-05 14:33:40 -0500

    Add routes to create/delete tags on posts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,12 @@ const postRoutes = require('./postRoutes');
 const userRoutes = require('./userRoutes');
 const responseRoutes = require('./responseRoutes');
 const upvoteRoutes = require('./upvoteRoutes');
+const tagRoutes = require('./tagRoutes');
 
 router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
 router.use('/responses', responseRoutes);
 router.use('/upvotes', upvoteRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;

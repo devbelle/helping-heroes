cebbe140d754f02a79f42be4ac0489595dde8a74
Devin <dbelle722@gmail.com>
2024-03-10 16:33:36 -0400

    edits

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -27,7 +27,7 @@ router.get('/', withAuth, async (req, res) => {
     ],
   });
   const posts = postData.map(val => val.get({ plain: true}));
-
+  console.log({posts})
   const responses = await Response.findAll({
     where: {user_id: req.session.user_id}
   });
@@ -39,10 +39,10 @@ router.get('/', withAuth, async (req, res) => {
   }
 
   res.render('dashboard', {
-    ...posts,
-    ...responses,
-    ...userUpvotes,
-    loggedIn: req.session.loggedIn,
+    posts,
+    responses,
+    userUpvotes,
+    logged_in: req.session.logged_in,
     is_admin: req.session.is_admin,
     username: req.session.username
   });
@@ -51,17 +51,75 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+//edit post
+router.get('/edit-post/:id', withAuth, async (req, res) => {
+  try{
+  const singlePost = await Post.findByPk(req.params.id, {
+    atttributes: [
+      'id',
+      'title',
+      'content'
+    ],
+    where: {user_id: req.session.user_id},
 
-//logout 
-router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
+    include: [
+      {
+        model: Response,
+        attributes: ['content'],
+      },
+      
+    ],
   });
+  const post = singlePost.map(val => val.get({ plain: true}));
+  
+
+  res.render('edit-post', {
+    post,
+    logged_in: req.session.logged_in,
+    is_admin: req.session.is_admin,
+    username: req.session.username
+  })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// //single-post
+// router.get('/single-post/:id', withAuth, async (req, res) => {
+//   try{
+//   const singlePost = await Post.findByPk(req.params.id, {
+//     atttributes: [
+//       'id',
+//       'title',
+//       'content'
+//     ],
+//     where: {user_id: req.session.user_id},
+
+//     include: [
+//       {
+//         model: Response,
+//         attributes: ['content'],
+//       },
+      
+//     ],
+//   });
+//   const post = singlePost.map(val => val.get({ plain: true}));
+//   const responses = await Response.findAll({
+//     where: {user_id: req.session.user_id}
+//   });
+  
+//   res.render('single-post', {
+//     post,
+//     responses,
+//     logged_in: req.session.logged_in,
+//     is_admin: req.session.is_admin,
+//     username: req.session.username
+//   })
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 
 
   

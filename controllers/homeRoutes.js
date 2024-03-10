d4176d1362bb34bc2d9c438d0c41a8684d7e28fe
D Pair <DESEANPAIR12@GMAIL.COM>
2024-03-10 10:37:43 -0400

    HomePage Updates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
-const { Post, Tag, User } = require('../models');
+const sequelize = require('../config/connection');
+const session = require('express-session');
+const { Post, Tag, User, TagPost } = require('../models');
 
 
 router.get('/', async (req, res) => {
   try {
+
     const postData = await Post.findAll({
-      attributes: ['title', 'content', 'id', ['created_at', 'date']],
+      attributes: ['title', 'id', ['created_at', 'date']],
       include: [ {
         model: User,
         attributes: ['username']
@@ -15,12 +18,52 @@ router.get('/', async (req, res) => {
         attributes: ['name']
       }
       ]
-   
-       // Include title and content from Post model
     });
 
-    const posts = postData.map(post => post.get({ plain: true }));
+    const postTags = [];
 
+    for (const post of postData) {
+    const postId = post.id;
+    
+    // Query the TagPost model to find tag associations for the post
+    const tagPostData = await TagPost.findAll({
+    where: {
+    post_id: postId
+    }
+    });
+
+
+
+  if (tagPostData.length > 0) {
+    // If tag associations are found, extract the tag ids
+    const tagIds = tagPostData.map(tagPost => tagPost.tag_id);
+
+    const tagNamesData = await Tag.findAll({
+      where: {
+        id: tagIds
+      }
+    });
+    
+    const tagNames = tagNamesData.map(tag => tag.name);
+
+    const postwithTag = {
+    id: postId,
+    tag: tagNames
+    };
+
+    
+    
+    postTags.push(postwithTag);
+      // Use the extracted tag ids as needed
+      console.log(`Post ID #${postId} is associated with the following tags:`, tagNames);
+    } else {
+      console.log("No tag associations found for the post.");
+    }
+    
+  }
+    
+  
+    const posts = postData.map(post => post.get({ plain: true }));
 
     const tagData = await Tag.findAll({
       attributes: ['name']
@@ -28,13 +71,17 @@ router.get('/', async (req, res) => {
 
     const tags = tagData.map(tag => tag.get({ plain: true }));
 
-
+   
+  
     res.render('homepage', {
       posts,
+      postTags,
       tags,
       logged_in: req.session.logged_in,
       username: req.session.username
     });
+
+    
 
   } catch (err) {
     console.log(err);
